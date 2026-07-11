a73585ffe5206a16ad94af97d23e3d6f4e9b8610
Tariq Yunusa <tariikhyunusa@gmail.com>
2026-07-11 10:40:28 +0100

    feat: added order model

---
+++ app/models/order.py
@@ -0,0 +1,23 @@
+import enum
+from datetime import datetime
+from decimal import Decimal
+from sqlalchemy import ForeignKey, Numeric, Enum as SQLEnum, func
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.database import Base
+
+class OrderStatus(str, enum.Enum):
+    PENDING = "pending"
+    CONFIRMED = "confirmed"
+    CANCELLED = "cancelled"
+    
+class Order(Base):
+    __tablename__ = "orders"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    ticket_type_id: Mapped[int] = mapped_column(ForeignKey("ticket_types.id"))
+    quantity: Mapped[int]
+    total_price: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+    status: Mapped[OrderStatus] = mapped_column(SQLEnum(OrderStatus, values_callable= lambda x: [e.value for e in x]), default=OrderStatus.PENDING),
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    
